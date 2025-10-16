7d68b7c716771a345b421020b2fd9335f80c1fba
Joselitonn2 <131082585+Joselitonn2@users.noreply.github.com>
2025-10-16 16:01:31 -0600

    Update toggleMenu.js

--- static/toggleMenu.js
+++ static/toggleMenu.js
@@ -1,10 +1,10 @@
 
   function toggleMenu(menuId) {
-    // 1. Alterna la clase 'show' solo en el menú clickeado
+   
     document.getElementById(menuId).classList.toggle("show");
   }
 
-  // Cierra los menús si se hace clic fuera de ellos (Mejora de UX)
+
   window.onclick = function(event) {
     if (!event.target.matches('.menu-btn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -16,4 +16,5 @@
         }
       }
     }
+
   }
