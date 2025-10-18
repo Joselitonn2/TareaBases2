4114e07b60c973373e8fa72cec02ae329c60f53f
Alel385 <127790152+Alel385@users.noreply.github.com>
2025-10-18 03:26:52 -0600

    Modify InsertarBitacora procedure for updates
    Updated the InsertarBitacora procedure to allow for larger descriptions and modified the PostInIP parameter length.

--- InsertarBitacora.sql
+++ InsertarBitacora.sql
@@ -1,12 +1,14 @@
-CREATE PROCEDURE InsertarBitacora
+CREATE OR ALTER PROCEDURE InsertarBitacora
   @IdTipoEvento INT,
-  @Descripcion  NVARCHAR(1000) = NULL,
+  @Descripcion  NVARCHAR(MAX) = NULL,
   @IdPostByUser INT = NULL,
-  @PostInIP     VARCHAR(45) = NULL
+  @PostInIP     VARCHAR(64) = NULL
 AS
 BEGIN
+
   SET NOCOUNT ON;
   INSERT INTO dbo.BitacoraEvento(IdTipoEvento, Descripcion, IdPostByUser, PostInIP)
   VALUES(@IdTipoEvento, @Descripcion, @IdPostByUser, @PostInIP);
+
 END
 GO
